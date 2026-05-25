84f241489f55397f9da0d1fa5bb4c5cddcc783b2
Omar Hasan <omarqasem.997@gmail.com>
2026-05-25 19:39:02 +0300

    🐛 FIX: remove hedless

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,11 +32,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-     headless:true,
-     
-     launchOptions: {
-  args: ['--disable-gpu']
-},
+
   },
 
   /* Configure projects for major browsers */
